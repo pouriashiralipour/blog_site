d609cf9387b5d9cce9a2d2f0c341c707f0c9b32f
Pouria <pouriashiralipour@gmail.com>
2023-11-06 11:51:42 +0330

    install pillow app for images fields

--- articles/admin.py
+++ articles/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from .models import Article
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'cover_img', 'status', 'publish', 'datetime_created', 'datetime_modified']
-    list_filter = ('publish', 'status', )
-    search_fields = ('title', 'description', )
+    list_filter = ('publish', 'status',)
+    search_fields = ('title', 'description',)
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['-status', '-publish']
