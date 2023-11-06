1c0847bef37e1802712c6bd5f38580977861941c
Pouria <pouriashiralipour@gmail.com>
2023-11-06 16:58:10 +0330

    show articles in templates

--- articles/views.py
+++ articles/views.py
@@ -7,7 +7,7 @@ from .models import Article
 class ArticleListView(generic.ListView):
     template_name = 'articles/article_list_view.html'
     model = Article
-    context_object_name = 'article'
+    context_object_name = 'articles'
 
 
 class ArticleDetailView(generic.DetailView):
