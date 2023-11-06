df9d6eff3c04f7813cf5830a5734d600bf687638
Pouria <pouriashiralipour@gmail.com>
2023-11-06 11:38:33 +0330

    translate accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
     # add custom fields
-    age = models.PositiveIntegerField(null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True, verbose_name=_('age'))
