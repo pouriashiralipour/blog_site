7dc0dab03eae48c8eb32dee4db1288e9bcae6260
Pouria <pouriashiralipour@gmail.com>
2023-11-06 16:42:35 +0330

    add urls and views of pages

--- pages/urls.py
+++ pages/urls.py
@@ -6,6 +6,6 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('', views.AboutUsView.as_view(), name='about_us'),
-    path('', views.ContactUsView.as_view(), name='contact_us'),
+    path('about_us/', views.AboutUsView.as_view(), name='about_us'),
+    path('contact_us', views.ContactUsView.as_view(), name='contact_us'),
 ]
