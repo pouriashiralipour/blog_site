8e1243c729d2261a13744e7ffd9675367730db8c
Pouria <pouriashiralipour@gmail.com>
2023-11-07 09:51:06 +0330

    add contact model to admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactUsModel
+
+
+@admin.register(ContactUsModel)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
