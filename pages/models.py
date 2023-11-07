34ec51de7bb10253ae97b508c0c9982a8530a602
Pouria <pouriashiralipour@gmail.com>
2023-11-07 09:47:26 +0330

    create model for cintact

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class ContactUsModel(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_('name'))
+    email = models.EmailField(verbose_name=_('email'))
+    topic = models.CharField(max_length=300, verbose_name=_('topic'))
+    text = models.TextField(verbose_name=_('text'))
