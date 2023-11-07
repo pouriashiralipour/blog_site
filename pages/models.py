ee9f41b95a1e65a33818fb848246b694d9f1dbb8
Pouria <pouriashiralipour@gmail.com>
2023-11-07 10:03:44 +0330

    change max length of topic

--- pages/models.py
+++ pages/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class ContactUsModel(models.Model):
     name = models.CharField(max_length=200, verbose_name=_('name'))
     email = models.EmailField(verbose_name=_('email'))
-    topic = models.CharField(max_length=300, verbose_name=_('topic'))
+    topic = models.CharField(max_length=500, verbose_name=_('topic'))
     text = models.TextField(verbose_name=_('text'))
     datetime_created = models.DateTimeField(default=timezone.now, verbose_name=_('datetime_created'), )
 
