9c326d6c324cd8c8c26f140a0361e4e4eee9b512
Pouria <pouriashiralipour@gmail.com>
2023-11-12 20:10:30 +0330

    add favorite articles to home page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
@@ -11,6 +12,16 @@ def home_page_view(request):
     offer_article = Article.objects.filter(status='p', offer_article=True).order_by('-publish')[1:]
     last_offer_article = Article.objects.filter(status='p', offer_article=True).order_by('-publish')[:1]
     hottest_article = Article.objects.filter(status='p', hottest_article=True).order_by('-publish')[:1]
+    favorite_article = Article.objects.filter(status='p').annotate(like_count=models.Count('likes')).order_by(
+        '-like_count')
+    favorite_article_index1 = Article.objects.filter(status='p').annotate(like_count=models.Count('likes')).order_by(
+        '-like_count')[:1]
+    favorite_article_index2 = Article.objects.filter(status='p').annotate(like_count=models.Count('likes')).order_by(
+        '-like_count')[1:2]
+    favorite_article_index3 = Article.objects.filter(status='p').annotate(like_count=models.Count('likes')).order_by(
+        '-like_count')[2:4]
+    favorite_article_index4 = Article.objects.filter(status='p').annotate(like_count=models.Count('likes')).order_by(
+        '-like_count')[4:6]
     articles = Article.objects.filter(status='p').order_by('-publish')
     category = Category.objects.filter(active=True).order_by('-datetime_created')
 
@@ -23,7 +34,10 @@ def home_page_view(request):
         news_form = NewsEmailForm
     context = {'articles': articles,
                'hottest_article': hottest_article, 'category': category, 'form': news_form,
-               'offer_article': offer_article, 'last_offer_article': last_offer_article}
+               'offer_article': offer_article, 'last_offer_article': last_offer_article,
+               'favorite_article': favorite_article, 'favorite_article_index1': favorite_article_index1,
+               'favorite_article_index2': favorite_article_index2, 'favorite_article_index3': favorite_article_index3,
+               'favorite_article_index4': favorite_article_index4}
     return render(request, 'pages/home_page.html', context)
 
 
